91d62af3aa13f3a1d415151c31e32c1046b3e889
Aymeric Biaou <marsa@Aymerics-MacBook-Air.local>
2023-09-29 10:02:17 +0200

    tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./dist/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     fontFamily: {
       Italianno: ["Italianno", "cursive"],
